db5bda185e14dfc0b9f6e1653df9cb3b750b85c1
NStein514 <nstein514@gmail.com>
2024-04-15 16:50:23 -0400

    Added Reloading & Reticle

--- Sausage Casualty/Assets/Gun/WeaponManager.cs
+++ Sausage Casualty/Assets/Gun/WeaponManager.cs
@@ -18,12 +18,16 @@ public class WeaponManager : MonoBehaviour
 
     [SerializeField] AudioClip gunShot;
     AudioSource audioSource;
+    WeaponAmmo ammo;
+    ActionStateManager actions;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         aim = GetComponentInParent<AimStateManager>();
+        ammo = GetComponent<WeaponAmmo>();
+        actions = GetComponentInParent<ActionStateManager>();
         fireRateTimer = fireRate;
     }
 
@@ -37,6 +41,8 @@ public class WeaponManager : MonoBehaviour
     {
         fireRateTimer += Time.deltaTime;
         if (fireRateTimer < fireRate) return false;
+        if (ammo.currentAmmo == 0) return false;
+        if (actions.currentState == actions.Reload) return false; 
         if (semiAuto && Input.GetKeyDown(KeyCode.Mouse0)) return true;
         if (!semiAuto && Input.GetKey(KeyCode.Mouse0)) return true;
         return false;
@@ -47,6 +53,7 @@ public class WeaponManager : MonoBehaviour
         fireRateTimer = 0;
         barrelPos.LookAt(aim.aimPos);
         audioSource.PlayOneShot(gunShot);
+        ammo.currentAmmo--;
         for (int i = 0; i < bulletsPerShot; i++)
         {
             GameObject currentBullet = Instantiate(bullet, barrelPos.position, barrelPos.rotation);
