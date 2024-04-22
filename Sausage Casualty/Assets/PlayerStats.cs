d9b7cfc69160e2e16ba66c2304d5787aa942f345
Rowan Knutsen <rk011616@ohio.edu>
2024-04-22 14:34:15 -0400

    Start Screen On Death
    When the player dies they return to the start screen.

--- Sausage Casualty/Assets/PlayerStats.cs
+++ Sausage Casualty/Assets/PlayerStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -16,6 +17,11 @@ public class PlayerStats : MonoBehaviour
         currentHealth = maxHealth;
 
         healthBar.SetSliderMax(maxHealth);
+    }
+
+    private void Update()
+    {
+        if (currentHealth <= 0) SceneManager.LoadScene("UI test");
     }
 
     public void TakeDamage(float damage)
